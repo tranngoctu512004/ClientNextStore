47dd46ba50e9d3772034d9bdedc12381aa1bb18e
tranngoctu512004 <130238498+tranngoctu05@users.noreply.github.com>
2024-08-06 16:31:41 +0700

    Update page.js

--- app/home/page.js
+++ app/home/page.js
@@ -1,11 +1,5 @@
-import Header from "@/components/header";
 import React from "react";
 
 export default function HomePage() {
-  return (
-    <div className="flex flex-row">
-      <Header />
-      Trang chủ
-    </div>
-  );
+  return <div className="flex flex-row">Trang chủ</div>;
 }
